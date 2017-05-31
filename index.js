11160e0f89f59a029c853b22da18d49549dc152f
Nicolas Le Roux <mr.leroux.nicolas@gmail.com>
2017-05-31 22:31:55 +0200

    Adjustment firebase database.

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 admin.initializeApp(functions.config().firebase);
 
+/*
 exports.onAddingNewEvent = functions.database.ref('/events/{eventId}')
 	.onWrite(event => {
 		// Grab the current value of what was written to the Realtime Database.
@@ -13,4 +14,4 @@ exports.onAddingNewEvent = functions.database.ref('/events/{eventId}')
 		// writing to the Firebase Realtime Database.
 		// Setting an "uppercase" sibling in the Realtime Database returns a Promise.
 		return event.data.ref.parent.child('uppercase').set(uppercase);
-	});
+	});*/
